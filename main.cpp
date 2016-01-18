a1534a84b5d890d516bb53cd81f3515a7077a8ed
Michail Tsyganov <tsyganov.michail@yandex.ru>
2016-01-18 22:16:25 +0300

    Приведено в состояние корректного вывода результатов.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,3 @@
-/* Вариант 6:
- * Очередь на основе односвязного списка, в которой хранится список пациентов (ФИО, возраст).
- *
- * Особенности:
- * Добавление только в конец и удаление только из начала
- *
- * Реализовать операции:
- *
- * 1. получить доступ к элементу по индексу (номеру);
- * 2. добавить новый элемент; (добавление только в конец) (CHECK!)
- * 3. удалить элемент по значению;
- * 4. объединить два набора в один;
- * 5. разбить один набор на несколько;
- * 6. определить число элементов; (CHECK!)
- * 7. создать копию.
-*/
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -49,17 +32,66 @@ int main()
     patientList *head = NULL;
     // Переменнная для дальнейших элементов списка
     // Индекс, по умолчанию равен 1
-    size_t index=0, num=0;
 
     while(data_list >> human.surname >> human.name >> human.patronymic >> human.age)
     {
-        index++;
         Patient person(human.surname, human.name, human.patronymic, human.age);
-        head=insertPatient(index, head,person);
+        head=insertPatient(head, person);
     }
-    //cout << index << endl;
-    countList(head, num);
+
+    // Тестирование операций:
+
+    // Вывод готового списка:
+    cout << "Вывод готового списка:" << endl;
     print(head);
+
+    // Подсчёт числа элементов в списке:
+    cout << "Подсчёт числа элементов в списке:" << endl;
+    size_t listsize=countList(head);
+    cout << "Элементов в списке: " << listsize << endl << endl;
+
+    // Поиск элемента в списке:
+    cout << "Поиск элемента в списке:" << endl;
+    patientList *check_search=searchPatient(head, 2, listsize);
+    cout << "Второй элемент списка: " << endl << check_search->person << endl << endl;
+
+    // Копирование списка:
+    cout << "Копирование списка:" << endl;
+    patientList *copy1=copyList(head); // для тестирования удаления
+    patientList *copy2=copyList(head); // для тестирования склейки
+    patientList *copy3=copyList(head); // для тестирования склейки
+    patientList *copy4=copyList(head); // для тестирования разделения
+    cout << "Копия списка:" << endl;
+    print(copy1);
+
+    // Удалить элемент по значению:
+    // Пациент, которого нет в списке
+    cout << "Удалить элемент по значению:" << endl;
+    Patient a("ffffff", "fffff", "ffffff", 18);
+    // Первый пациент в очереди
+    Patient b("Fmhsaxtljj", "Cwpatrwcym", "Bhpshnfyji", 86);
+    patientList *check_delete;
+    cout << a << endl;
+    check_delete = deletePatient(copy1, a);
+    cout << endl << b << endl;
+    check_delete = deletePatient(copy1, b);
+    cout << endl;
+    print(check_delete);
+
+    // Объединить 2 списка в 1:
+    cout << "Объединить 2 списка в 1:" << endl;
+    patientList *combine=combineLists(copy2, copy3);
+    print(combine);
+
+    // Разделить список на 2 части:
+    cout << "Разделить список на 2 части:" << endl;
+    patientList *copy5=NULL, *copy6=NULL;
+    divideList(copy4, 3, copy5, copy6);
+    cout << "1 часть:" << endl;
+    print(copy5);
+    cout << "2 часть:" << endl;
+    print(copy6);
+
     return 0;
 }
 
